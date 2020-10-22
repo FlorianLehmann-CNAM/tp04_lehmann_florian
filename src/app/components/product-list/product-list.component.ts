bda6a3e07526cee53a4a2e6391c19c7afaeeed0c
FlorianLehmann-CNAM <lehmann.florian.pro@gmail.com>
2020-10-22 15:06:35 +0200

    2 files modified

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,29 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/internal/Observable';
-import { Product } from '../../models/Product';
-import { HttpServiceService } from '../../services/http-service.service';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs/internal/Observable";
+import { Product } from "../../models/Product";
+import { HttpServiceService } from "../../services/http-service.service";
+import { of } from "rxjs";
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"]
 })
 export class ProductListComponent implements OnInit {
+  productsObservable: Observable<Product[]>;
 
+  displayProducts: Observable<Product[]>;
 
-  productsObservable : Observable<Product[]>;
-
-  displayProducts : Product[];
-
-  constructor(private httpService : HttpServiceService) { }
+  constructor(private httpService: HttpServiceService) {}
 
   ngOnInit() {
     this.productsObservable = this.httpService.getProductData();
-    this.productsObservable.subscribe((value : Product[]) => this.displayProducts = value);
+    this.productsObservable.subscribe(
+      (value: Product[]) => (this.displayProducts = of(value))
+    );
   }
 
-  onFilteredData(event : Product[]) : void{
-    this.displayProducts = event;
+  onFilteredData(event: Product[]): void {
+    this.displayProducts = of(event);
   }
-
 }

0b222c7ec32e2bcee085edf61d5ab2a9e7e81fa9
FlorianLehmann-CNAM <lehmann.florian.pro@gmail.com>
2020-10-24 18:40:44 +0200

    Fix pour stackblitz vu que c'est de la grosse merde

--- src/app/modules/catalog/search-box/search-box.component.ts
+++ src/app/modules/catalog/search-box/search-box.component.ts
@@ -1,37 +1,36 @@
-import { Component, EventEmitter,  Input,  OnInit, Output } from '@angular/core';
-import { Observable } from 'rxjs/internal/Observable';
-import { map } from 'rxjs/operators';
-import { Product } from '../../../models/Product';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Observable } from "rxjs/internal/Observable";
+import { map } from "rxjs/operators";
+import { Product } from "../../../models/Product";
 
 @Component({
-  selector: 'app-search-box',
-  templateUrl: './search-box.component.html',
-  styleUrls: ['./search-box.component.css']
+  selector: "app-search-box",
+  templateUrl: "./search-box.component.html",
+  styleUrls: ["./search-box.component.css"]
 })
 export class SearchBoxComponent implements OnInit {
-
-  @Input() dataToFilter : Observable<Product[]>;
-  @Output() onFilteredData : EventEmitter<Observable<Product[]>>
+  @Input() dataToFilter: Observable<Product[]>;
+  @Output() onFilteredData: EventEmitter<Observable<Product[]>>;
 
   private isTyping = false;
 
   constructor() {
     this.onFilteredData = new EventEmitter<Observable<Product[]>>();
-
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  onKey(value : string) : void{
-    
-    console.log(value);
+  onKey(event: Event): void {
     let output$ = this.dataToFilter.pipe(
-      map((products : Product[]) => products.filter((p) => p.title.toLowerCase().includes(value.toLowerCase())))
-    )
-    
+      map((products: Product[]) =>
+        products.filter(p =>
+          p.title
+            .toLowerCase()
+            .includes((event.target as HTMLInputElement).value.toLowerCase())
+        )
+      )
+    );
+
     this.onFilteredData.emit(output$);
-
   }
-
 }
--- src/app/modules/shopping-cart/shopping-cart/shopping-cart.component.ts
+++ src/app/modules/shopping-cart/shopping-cart/shopping-cart.component.ts
@@ -1,46 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngxs/store';
-import { Observable, of } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
-import { ClearShoppingCart, DelProduct, ModifyQuantity } from '../../../store/actions/ShoppingCart.action';
-import { Product } from '../../../models/Product';
-import { Article } from '../../../models/ShoppingCart';
-import { ShoppingCartState } from '../../../store/states/ShoppingCart.state';
+import { Component, OnInit } from "@angular/core";
+import { Store } from "@ngxs/store";
+import { Observable, of } from "rxjs";
+import { filter, map } from "rxjs/operators";
+import {
+  ClearShoppingCart,
+  DelProduct,
+  ModifyQuantity
+} from "../../../store/actions/ShoppingCart.action";
+import { Product } from "../../../models/Product";
+import { Article } from "../../../models/ShoppingCart";
+import { ShoppingCartState } from "../../../store/states/ShoppingCart.state";
 
 @Component({
-  selector: 'app-shopping-cart',
-  templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.css']
+  selector: "app-shopping-cart",
+  templateUrl: "./shopping-cart.component.html",
+  styleUrls: ["./shopping-cart.component.css"]
 })
 export class ShoppingCartComponent implements OnInit {
+  products$: Observable<Article[]>;
 
-    products$ : Observable<Article[]>;
+  constructor(private store: Store) {}
 
-    constructor(private store: Store) { }
+  ngOnInit(): void {
+    this.products$ = this.store.select(ShoppingCartState.GetProducts);
+  }
 
-    ngOnInit(): void {
-        this.products$ = this.store.select(ShoppingCartState.GetProducts)
-    }
+  getTotalPriceFromProduct(product: Article): number {
+    return product.price * product.quantity;
+  }
 
-    getTotalPriceFromProduct(product: Article) : number{
-        return product.price * product.quantity;
-    }
-    
-    getShoppingCartTotalPrice() : Observable<number>{
-        return this.products$.pipe(
-            map((value: Article[]) => value.map((p : Article) => p.quantity * p.price).reduce((acc, value) => acc + value, 0)),
-        )
-    }
+  getShoppingCartTotalPrice(): Observable<number> {
+    return this.products$.pipe(
+      map((value: Article[]) =>
+        value
+          .map((p: Article) => p.quantity * p.price)
+          .reduce((acc, value) => acc + value, 0)
+      )
+    );
+  }
 
-    clearShoppingCart() : void{
-        this.store.dispatch(new ClearShoppingCart());
-    }
+  clearShoppingCart(): void {
+    this.store.dispatch(new ClearShoppingCart());
+  }
 
-    deleteProduct(product : Article) : void{
-        this.store.dispatch(new DelProduct(product));
-    }
+  deleteProduct(product: Article): void {
+    this.store.dispatch(new DelProduct(product));
+  }
 
-    onQuantityChanged(product: Article, newQuantity : number){
-        this.store.dispatch(new ModifyQuantity(product, newQuantity));
-    }
+  onQuantityChanged(product: Article, newQuantity: Event) {
+    this.store.dispatch(
+      new ModifyQuantity(
+        product,
+        Number((newQuantity.target as HTMLInputElement).value)
+      )
+    );
+  }
 }
